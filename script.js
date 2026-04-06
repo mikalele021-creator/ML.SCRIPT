db6ea751ba08d3fb13c6b1ccf498cf1bb8c00cab
mikalele021-creator <mikalele021@gmail.com>
2026-04-05 22:08:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,22 @@
 
 //SEGUNDA
 
-let soma = 0
-for(let i = 1; i <=5; i ++){
-soma=i+=1
-console.log(i)
+//let soma = 0
+//for(let i = 1; i <=5; i ++){
+//soma=i+=1
+//console.log(i)
+//}
+
+
+//TERCEIRA
+
+//for(let i = 1; i<= 15; i += 2){
+  //  console.log(i)
+//}
+
+
+//QUARTA
+
+for (let i = 0; i <= 20; i += 2){
+    console.log(i)
 }
